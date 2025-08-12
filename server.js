9070409e03a912019cb2dfab876128292b54077f
Kajaluxan <kajaluxan2002@gmail.com>
2025-08-12 09:58:05 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';                    
+
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//enable CORS for all origins (for dev)
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
